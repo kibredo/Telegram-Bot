e5f3b699998d9b03e18d96c61ff6f3c0365b6f62
kibredo <146701489+kibredo@users.noreply.github.com>
2023-12-02 22:26:30 +0300

    Update thing.py

--- src/thing.py
+++ src/thing.py
@@ -19,20 +19,20 @@ def safename(name):
 	return result
 
 def main():
-	while True:
-	    time.sleep(1)
-	    start_time = time.time()
-	    updates = requests.get(f'{API_URL}{SECRET_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
-	
-	    if updates['result']:
-	
-	        command = str(updates['result'][0]['message']['text'])
-	        id = str(updates['result'][0]['message']['from']['id'])
-	        send_to_telegram(id, command)
-	        
-	        for result in updates['result']:
-	            offset = result['update_id']
-	            
-	
-	    end_time = time.time()
-	    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
+    while True:
+        time.sleep(1)
+        start_time = time.time()
+        updates = requests.get(f'{API_URL}{SECRET_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+
+        if updates['result']:
+
+            command = str(updates['result'][0]['message']['text'])
+            id = str(updates['result'][0]['message']['from']['id'])
+            send_to_telegram(id, command)
+            
+            for result in updates['result']:
+                offset = result['update_id']
+                
+
+        end_time = time.time()
+        print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
