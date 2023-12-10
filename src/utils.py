ff90df2091d5936af1b8cb0d6d6169a4aa236df2
kibredo <146701489+kibredo@users.noreply.github.com>
2023-12-11 00:15:28 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,25 @@
 from datetime import datetime
 import http.client
+import json
 
 from src.data import DATA
 from src.data import HEADER
 from src.data import schedule
 from src.data import URL
 
-
+updates = dict()
 def send_to_telegram(id, command):
     ans = str()
 
     if command == "/getmonday":
         ans = "Monday"
 
-    elif command == "/getthuesday":
+    elif command == "/gettuesday":
         ans = "Thuesday"
 
     elif command == "/getwednesday":
         ans = "Wednesday"
-        
+
     elif command == "/getthursday":
         ans = "Thursday"
 
@@ -30,7 +31,16 @@ def send_to_telegram(id, command):
 
     elif command == "/help":
         ans = "Help"
-
+    elif command[7:9] == "mo":
+        ans = "cMonday"
+    elif command[7:9] == "tu":
+        ans = "cThuesday"
+    elif command[7:9] == "we":
+        ans = "cWednesday"
+    elif command[7:9] == "th":
+        ans = "cThursday"
+    elif command[7:9] == "fr":
+        ans = "cFriday"
     else:
         final = str()
         current = datetime.today().weekday()
@@ -59,8 +69,23 @@ def send_to_telegram(id, command):
 
         ans = final
 
-    t = DATA.replace('message', schedule[ans])
-    t = t.replace('ID', id)
+    if ans[0] != 'c':
+        t = DATA.replace('message', schedule[ans])
+        t = t.replace('ID', id)
+
+    else:
+        arr = command.splitlines()
+        final_string = str()
+        final_string = '\n'.join(arr[1:])
+        schedule[ans[1:]] = final_string
+        final = "ready\n"
+        t = DATA.replace('message', schedule[ans[1:]])
+        t = t.replace('ID', id)
+        #with open("ans.json", "w") as tans:
+        #    tans.write(json.dumps(schedule, indent=2))
+        #    tans.write(command)
+            
+
     t_header = dict(HEADER)
     httpscon = http.client.HTTPSConnection('api.telegram.org')
     t_header['Connection'] = 'close'
